d4d4bcb7dea033d0a3f5ceae311780b0b9d30365
EmeryP <emeryparks09@hotmail.com>
2018-04-25 13:52:28 -0700

    worked on server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 
 
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/postgres');
 client.connect();
 client.on('error', err => console.error(err));
 
 app.use(cors());
 
-app.get('/', (req, res) => res.redirect(CLIENT_URL));
+// app.get('/', (req, res) => res.redirect(CLIENT_URL));
 
 app.get('/test/', (req, res) => res.send('Welcome to our data'));
 
