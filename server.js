01aa44ae676d06f6318d794b20416e1ea019a9c1
EmeryP <emeryparks09@hotmail.com>
2018-04-27 00:03:02 -0700

    added detail book vie

--- server.js
+++ server.js
@@ -8,14 +8,13 @@ const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 
-
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/postgres');
 client.connect();
 client.on('error', err => console.error(err));
 
 app.use(cors());
 
-// app.get('/', (req, res) => res.redirect(CLIENT_URL));
+app.get('/', (req, res) => res.redirect(CLIENT_URL));
 
 app.get('/test/', (req, res) => res.send('Welcome to our data'));
 
@@ -28,10 +27,29 @@ app.get('/api/v1/books', (req, res) => {
 });
 
 app.get('/api/v1/books/:id', (req, res) => { //placeholder for any id being passed in
-  client.query('SELECT * FROM books WHERE book_id = $1;', [req.params.id])
-    .then(results => res.send(results.rows))
+  client.query('SELECT * FROM books WHERE id = $1', [req.params.id])
+    .then(results => res.send(results.rows[0]))
     .catch(console.error);
 });
+
+app.post('/api/v1/books', (req, res) => {
+  console.log(req.body.book);
+  let insert = 'INSERT INTO books (title, author, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5)';
+  let values = [req.body.title,
+    req.body.author,
+    req.body.isbn,
+    req.body.image_url,
+    req.body.description];
+
+  client.query(insert, values)
+    .then(results => res.json(results))
+    .catch(err => {
+      console.error(err)
+      res.sendStatus(500).send("error");
+    });
+});
+
+// app.put()
 
 app.get('*', (req, res) => res.redirect('404 service not found'));
 
